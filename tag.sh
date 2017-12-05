f578c40ea4a501de8587d1af9382019474d160cc
Michael Corrigan <corrigan@withjoy.com>
2017-12-05 14:26:42 -0800

    release fix

--- tag.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-set -e
-GITURL="https://github.com/joylifeinc/joy_kit.git"
-npm version patch
-TAG=v$(node -p -e "require('./package.json').version")
-git push "$GITURL" "$TAG"
-echo "$TAG >> $GITURL"
