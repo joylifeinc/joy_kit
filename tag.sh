8b676a4270ad4a5903439716a5cb7673751b4aae
Michael Corrigan <corrigan@withjoy.com>
2017-12-05 13:43:54 -0800

    fix build script

--- tag.sh
+++ tag.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -e
-GITURL="https://bitbucket.org/joylife/sdk-js.git"
+GITURL="https://github.com/joylifeinc/joy_kit.git"
 npm version patch
 TAG=v$(node -p -e "require('./package.json').version")
 git push "$GITURL" "$TAG"
