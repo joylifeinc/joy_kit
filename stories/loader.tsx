d0d334260c8141f0f425dea13ab7d9438815cff0
Kevin Tran <kevin@withjoy.com>
2017-12-06 12:11:18 -0800

    Add spinnie loader

--- stories/loader.tsx
+++ stories/loader.tsx
@@ -1,19 +1,36 @@
 import * as React from 'react';
 import { addDecorator, storiesOf } from '@storybook/react';
-import {
-  withKnobs,
-  text,
-  boolean,
-  number,
-  select
-} from '@storybook/addon-knobs';
+import { withKnobs, text, color, number } from '@storybook/addon-knobs';
 
 import { SyntaxHighlight } from './utils/syntax';
-import { SpinnerLoader } from '../src';
+import { SpinnerLoader, Fragments } from '../src';
 
 const stories = storiesOf('Loaders', module);
 stories.addDecorator(withKnobs);
 
 stories.add('Spinner', () => {
-  return <SpinnerLoader />;
+  const height = text('Height', '50px');
+  const width = text('Width', '50px');
+  const borderColor = color('Color', 'black');
+  const thickness = text('Thickness', '2px');
+
+  return (
+    <div>
+      <SpinnerLoader
+        height={height}
+        width={width}
+        color={borderColor}
+        thickness={thickness}
+      />
+      <SyntaxHighlight
+        syntax={'jsx'}
+        codeblock={`<SpinnerLoader 
+          height={"${height}"} 
+          width={"${width}"} 
+          color={"${borderColor}"} 
+          thickness={"${thickness}"} 
+        />`}
+      />
+    </div>
+  );
 });
